e1463b5a771dc838783cc4a0bc5e628374955b0f
Amalia Claire Fergin <acfergin@msn.com>
2017-04-11 10:58:34 -0700

    added single movies information -- Get request:id

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var movies = require('./routes/movies');
+
 // var moviesData = require('./seeds/1_movies.js')
 
 var app = express();
--- routes/movies.js
+++ routes/movies.js
@@ -6,7 +6,18 @@ var db = require('../db')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   db('movies').then(movies =>{
-    res.render('movies/index', {movies});
+    res.render('movies/index', {movies}); // movies/index.hbs section
+  })
+
+});
+
+router.get('/:id', function(req, res, next) {
+  let id = req.params.id
+  db('movies').select('id','title', 'director', 'year', 'my_rating', 'poster_url')
+  .where({
+    'id': id
+  }).then(movies =>{
+    res.render('movies/details', {amalia : movies[0]}); // movies/index.hbs section
   })
 
 });
