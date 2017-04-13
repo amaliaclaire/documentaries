3a545875fb62eb3401275c763e28d82c2e3ea682
Amalia Claire Fergin <acfergin@msn.com>
2017-04-13 11:09:29 -0700

    last bit with wes

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
-const env = 'development';
+const env = process.env.NODE_ENV || 'development';
+// NODE_ENV heroku defaults this to production 
 const config = require('../knexfile.js')[env];
+// brackets allows the value of ENV to be used rather than hardcoding OBJECT at env.
 const knex = require('knex')(config);
 
-module.exports = knex; 
+module.exports = knex;
