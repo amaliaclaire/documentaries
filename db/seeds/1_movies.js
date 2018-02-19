2dae369f0f61951c8ad6e09acd34cf316c4d4c67
Amalia Claire Fergin <acfergin@msn.com>
2018-02-19 15:28:05 -0800

    deleted nav

--- db/seeds/1_movies.js
+++ db/seeds/1_movies.js
@@ -4,27 +4,43 @@ exports.seed = (knex) => {
       return knex('movies').insert([
         {
           id: 1,
-          title: 'Speed 2: Cruise Control',
-          director: 'Jan de Bont',
-          year: 1997,
+          title: 'Jesus Camp',
+          director: 'Rachel Grady',
+          year: 2006,
           my_rating: 3,
-          poster_url: 'http://www.gstatic.com/tv/thumb/movieposters/19492/p19492_p_v8_ae.jpg'
+          poster_url: 'http://www.gstatic.com/tv/thumb/dvdboxart/163609/p163609_d_v8_aa.jpg'
         },
         {
           id: 2,
-          title: 'Highlander II: The Quickening',
-          director: 'Russell Mulcahy',
-          year: 1991,
-          my_rating: 1,
-          poster_url: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Highlander_II.jpg'
+          title: 'Hot Girls Wanted',
+          director: 'Ronna Gradus',
+          year: 2015,
+          my_rating: 3,
+          poster_url: 'http://www.gstatic.com/tv/thumb/movieposters/11464938/p11464938_p_v8_aa.jpg'
         },
         {
           id: 3,
-          title: 'Spice World',
-          director: 'Bob Spiers',
-          year: 1998,
+          title: 'Hurt Locker',
+          director: 'Kathryn Bigelow',
+          year: 2009,
+          my_rating: 3,
+          poster_url: 'http://www.gstatic.com/tv/thumb/movieposters/197175/p197175_p_v8_ah.jpg'
+        },
+        {
+          id: 4,
+          title: 'White Helmets',
+          director: 'James Le Mesurier',
+          year: 2014,
           my_rating: 5,
-          poster_url: 'http://www.gstatic.com/tv/thumb/movieposters/20358/p20358_p_v8_aa.jpg'
+          poster_url: 'http://filmmusicreporter.com/wp-content/uploads/2016/11/the-white-helmets.jpg'
+        },
+        {
+          id: 5,
+          title: 'The Act Of Killing',
+          director: 'Joshua Oppenheimer',
+          year: 2013,
+          my_rating: 5,
+          poster_url: 'https://upload.wikimedia.org/wikipedia/en/c/ca/The_Act_of_Killing_%282012_film%29.jpg'
         }
       ])
     }).then(() => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Amalia's Movie Rating" });
+  res.render('index', { title: "Favorite Documentaries" });
 });
 
 module.exports = router;
